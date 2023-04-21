139c7e58904fb01022c1b854716c0bd0454369ef
Ariel Williams <arielwilliams121013@gmail.com>
2023-04-20 23:30:37 -0700

    Refactored handle_book() by creating helper func validate_book() that handles non-existing book and handles invalid book_id. Then called helper func in handle_book()

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -15,6 +15,7 @@ books = [
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+# func gets all books as a list of dicts where each book is a dict
 @books_bp.route("", methods=["GET"])
 def handle_books():
     books_response = []
@@ -28,13 +29,53 @@ def handle_books():
         )
     return jsonify(books_response)
 
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    book_id = int(book_id)
+###### Note: refactored version below #######
+# # Does all 3 of these things: grabs 1 book dict, 
+# # handles non-existent book w/ 404, & string that can't be transformed
+# # into an int w/ 400
+# # if if check evaluates to False, func returns None     
+# @books_bp.route("/<book_id>", methods=["GET"])
+# def handle_book(book_id):
+#     try:
+#         book_id = int(book_id)
+#     except:
+#         return {"message":f"book {book_id} invalid"}, 400
+    
+#     # book_id = int(book_id)
+#     for book in books:
+#         if book.id == book_id:
+#             return jsonify({
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description,
+#             })
+        
+#     return {"message":f"book {book_id} not found"}, 404
+
+
+### Refactored function from above by using helper function ###
+
+# helper function for error handling logic that will be used later
+# validate_book helper func: 1. handles non-existing book, 2. handles invalid book_id
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
     for book in books:
         if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
-            }
+            return book
+
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+# Refactored handle_book(book_id) func 
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
