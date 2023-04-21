f2ba5a06ecd380f320296ef8b9db329c73dbf6d7
Ariel Williams <arielwilliams121013@gmail.com>
2023-04-20 22:52:17 -0700

    created Book class

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,12 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     # Register Blueprints here
+    # Blueprint for hello_word_bp
     from .routes import hello_world_bp
     app.register_blueprint(hello_world_bp)
+    # Blueprint for books_bp
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
 
     return app
+
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 hello_world_bp = Blueprint("hello_world_bp", __name__)
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-
+# .route means endpoint is created 
 @hello_world_bp.route("/hello-world", methods=["GET"])
 def say_hello_world():
     return "Hello, World!"
@@ -26,5 +27,46 @@ def broken_endpoint():
         "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
     }
     new_hobby = "Surfing"
+    # fixed below line that was broken
+    # response_body["hobbies"] + new_hobby
     response_body["hobbies"].append(new_hobby)
     return response_body
+
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+    Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+] 
+
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response)
+
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = int(book_id)
+
+    for book in books:
+        if book.id == book.id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
