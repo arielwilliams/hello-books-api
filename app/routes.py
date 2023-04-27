9bcf0773f0be7b0ffd3238087d36472aff2d8349
Ariel Williams <arielwilliams121013@gmail.com>
2023-04-27 14:56:35 -0700

    Successfully implemented create_book to POST, and read_all_books to GET.

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,25 @@
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.book import Book
 
-from flask import Blueprint, jsonify, abort, make_response
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+@books_bp.route("", methods=["POST"])
+def create_book():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
 
-books = [
-    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-]
+    db.session.add(new_book)
+    db.session.commit()
 
-books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+    return make_response(f"Book {new_book.title} successfully created", 201)
 
-# func gets all books as a list of dicts where each book is a dict
+
 @books_bp.route("", methods=["GET"])
-def handle_books():
+def read_all_books():
     books_response = []
+    books = Book.query.all()
     for book in books:
         books_response.append(
             {
@@ -29,53 +30,3 @@ def handle_books():
         )
     return jsonify(books_response)
 
-###### Note: refactored version below #######
-# # Does all 3 of these things: grabs 1 book dict, 
-# # handles non-existent book w/ 404, & string that can't be transformed
-# # into an int w/ 400
-# # if if check evaluates to False, func returns None     
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except:
-#         return {"message":f"book {book_id} invalid"}, 400
-    
-#     # book_id = int(book_id)
-#     for book in books:
-#         if book.id == book_id:
-#             return jsonify({
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description,
-#             })
-        
-#     return {"message":f"book {book_id} not found"}, 404
-
-
-### Refactored function from above by using helper function ###
-
-# helper function for error handling logic that will be used later
-# validate_book helper func: 1. handles non-existing book, 2. handles invalid book_id
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        abort(make_response({"message":f"book {book_id} invalid"}, 400))
-
-    for book in books:
-        if book.id == book_id:
-            return book
-
-    abort(make_response({"message":f"book {book_id} not found"}, 404))
-
-# Refactored handle_book(book_id) func 
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    book = validate_book(book_id)
-
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description,
-    }
