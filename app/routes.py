c5a41b4ddf4305b7adda183aecf2ccb808255e4c
Ariel Williams <arielwilliams121013@gmail.com>
2023-04-30 21:42:19 -0700

    Created three functions and one helper function: read_one_book() that uses GET, update_book() that uses PUT, and delete_book that uses DELETE. Successfully deleted book with id:1

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.models.book import Book
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+# creates new book
 @books_bp.route("", methods=["POST"])
 def create_book():
     request_body = request.get_json()
@@ -15,7 +16,7 @@ def create_book():
 
     return make_response(f"Book {new_book.title} successfully created", 201)
 
-
+# reads/gets all books
 @books_bp.route("", methods=["GET"])
 def read_all_books():
     books_response = []
@@ -30,3 +31,54 @@ def read_all_books():
         )
     return jsonify(books_response)
 
+
+
+# helper function for read_one_book() AND update_book() that returns 400 and 404 
+# messages if book does not exist; and if it does exist it returns the book
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    book = Book.query.get(book_id)
+
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+    return book
+
+# reads/gets specific existing book, if it does not exist returns 400 or 404
+@books_bp.route("/<book_id>", methods=["GET"])
+def read_one_book(book_id):
+    book = validate_book(book_id)
+    return {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        }
+
+# puts/updates specific existing book, if it does not exist returns 400 or 404
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
+
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully updated")
+
+
+# deletes specific existing book, if it does not exist returns 400 or 404
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully deleted")
