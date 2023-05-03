e04e1b11ca04dbde3380c1c9d34731121e70e7cb
Ariel Williams <arielwilliams121013@gmail.com>
2023-05-03 11:19:47 -0700

    Created tests folder with three files. Wrote tests in test_routes.py and code in conftest.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,63 @@
+# code below handles development AND test modes
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.models.book import Book
+
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
+
+    return app
+
+
+
+
+
+# # code below only handles development mode
 # from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+# db = SQLAlchemy()
+# migrate = Migrate()
 
 
 # def create_app(test_config=None):
 #     app = Flask(__name__)
 
-#     # Register Blueprints here
+#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+
+#     db.init_app(app)
+#     migrate.init_app(app, db)
+
+#     # line below ensures Book model will be available to the app when we update database
+#     from app.models.book import Book
 #     from .routes import books_bp
+
 #     app.register_blueprint(books_bp)
 
 #     return app
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-migrate = Migrate()
-
-
-def create_app(test_config=None):
-    app = Flask(__name__)
-
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    # line below ensures Book model will be available to the app when we update database
-    from app.models.book import Book
-    from .routes import books_bp
-
-    app.register_blueprint(books_bp)
-
-    return app
--- app/routes.py
+++ app/routes.py
@@ -4,58 +4,7 @@ from app.models.book import Book
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-# creates new book
-@books_bp.route("", methods=["POST"])
-def create_book():
-    request_body = request.get_json()
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
-
-    db.session.add(new_book)
-    db.session.commit()
-
-    return make_response(f"Book {new_book.title} successfully created", 201)
-
-# # reads/gets all books
-# @books_bp.route("", methods=["GET"])
-# def read_all_books():
-#     books_response = []
-#     books = Book.query.all()
-#     for book in books:
-#         books_response.append(
-#             {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
-#         )
-#     return jsonify(books_response)
-
-
-# refactored reads/gets all books to include quary param
-@books_bp.route("", methods=["GET"])
-def read_all_books():
-
-    # this code replaces the previous query all code
-    title_query = request.args.get("title")
-    if title_query:
-        books = Book.query.filter_by(title=title_query)
-    else:
-        books = Book.query.all()
-    # end of the new code
-
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
-
-    return jsonify(books_response)
-
-
-
+####### helper functions here #######
 # helper function for read_one_book() AND update_book() that returns 400 and 404 
 # messages if book does not exist; and if it does exist it returns the book
 def validate_book(book_id):
@@ -70,6 +19,46 @@ def validate_book(book_id):
         abort(make_response({"message":f"book {book_id} not found"}, 404))
 
     return book
+###############################################################################
+
+########## Routes here #########
+
+# creates new book
+@books_bp.route("", methods=["POST"])
+def create_book():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(jsonify(f"Book {new_book.title} successfully created"), 201)
+    # return statement below to make our tests fail
+    # return make_response("I'm a teapot!", 418)
+
+
+# refactored reads/gets all books to include query param title
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    title_query = request.args.get("title")
+    if title_query:
+        books = Book.query.filter_by(title=title_query)
+    else:
+        books = Book.query.all()
+
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+
+    return jsonify(books_response), 200 
+    # return statement below to make our tests fail
+    # return make_response("I'm a teapot!", 418)
+
 
 # reads/gets specific existing book, if it does not exist returns 400 or 404
 @books_bp.route("/<book_id>", methods=["GET"])
@@ -80,6 +69,7 @@ def read_one_book(book_id):
             "title": book.title,
             "description": book.description
         }
+
 
 # puts/updates specific existing book, if it does not exist returns 400 or 404
 @books_bp.route("/<book_id>", methods=["PUT"])
@@ -93,7 +83,7 @@ def update_book(book_id):
 
     db.session.commit()
 
-    return make_response(f"Book #{book.id} successfully updated")
+    return make_response(jsonify(f"Book #{book.id} successfully updated"))
 
 
 # deletes specific existing book, if it does not exist returns 400 or 404
@@ -104,4 +94,5 @@ def delete_book(book_id):
     db.session.delete(book)
     db.session.commit()
 
-    return make_response(f"Book #{book.id} successfully deleted")
+    return make_response(jsonify(f"Book #{book.id} successfully deleted"))
+
